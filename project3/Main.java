280707f71e173bdd4aaa48b774a791458fae3ec4
root <root@Ubuntu.myguest.virtualbox.org>
2023-08-08 07:47:48 -0400

    changes

---
+++ project3/Main.java
@@ -0,0 +1,10 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    //Xkcdpwgen x = new Xkcdpwgen();
+    public static void main(String[] args) {
+        xkcdpwgen x = new xkcdpwgen();
+       x.createPassword1("-h");
+
+        }
+    }
